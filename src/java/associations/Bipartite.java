5d7715d2c5e1ddad338133e9ca2ca191570d69cf
Paul Anderson <ande9474@umn.edu>
2014-01-29 14:15:12 +0100

    Cleaning, organization of code
    Matching strategy (size or type choice) can now be selected by changing
    value of "UtilFunc". Both strategies can now be run without need to
    comment/uncomment any code.

--- src/java/associations/Bipartite.java
+++ src/java/associations/Bipartite.java
@@ -2,87 +2,79 @@ package Hungarian;
 
 import java.io.*;
 import java.util.*;
-/*
- * created by: p anderson, epfl
- * on: 22 october, 2013
- * last edited by: p anderson, epfl
- * on: 15 november, 2013
- * summary: 
- * comments:
- */
- 
+
 class Bipartite {
 
 public static void main(String[] args){
 //INSTRUCTIONS: compile package with javac -classpath . Hungarian/Bipartite.java
 //then, run with java -Xmx1024m Hungarian/Bipartite
 
-File hhldfile = new File ("Households1004updated.csv");
-// File hhldfile = new File ("July_SizeChoice/Households1004wHead.csv"); //for spouse matching
-int hhldwidth=12; //for head matching
-// int hhldwidth=18; //for spouse matching
-File pfile = new File ("People1011Matchingrandom.txt");
-// File pfile = new File ("July_SizeChoice/UnmatchedPeople.csv"); //for spouse matching
-String ResultsFileName="July_TypeChoice/Matching_results.txt"; //for head matching
-// String ResultsFileName="July_SizeChoice/Spouse_results.txt";//for spouse matching
+//<INPUT FILES>
+	//household file
+	File hhldfile = new File ("Households1004updated.csv");
+	int hhldwidth=12; //number of columns in data
+	
+	//person file
+	File pfile = new File ("People1011Matchingrandom.txt");
 
-int hhldfilelines, pfilelines, i, j;
-String temp, currentline[];
-Bipartite classcall = new Bipartite(); //run non-static methods
+	//results file
+	String ResultsFileName="July_TypeChoice/Matching_results.txt"; //for head matching
+//</INPUT FILES>
 
-hhldfilelines=(classcall.LineNo(hhldfile)-1); //subtract 1 because we have headers
-pfilelines=(classcall.LineNo(pfile)-1);
+//<Utility function constants>
+double asc[]=new double[3];
+double educ[]=new double[4];
+double age[]=new double[8];
+double workers[]=new double[7];
+double cars[]=new double[5];
 
-System.out.println("Number of households: "+hhldfilelines+"\nNumber of persons: "+pfilelines+"\n");
+//choose utility function, sizechoice=0, typechoice=1
+int UtilFunc=0; 
 
-//alternative specific constants for utility function
-//householdchoice.mod
-// double[] asc={3.34};
+	//sizechoice.mod
+		if(UtilFunc==0){
+		asc=new double[]{1.91,0,-0.737,-0.993,-1.86,-2.63};
+		educ=new double[]{0,0.150,0.150,-0.657};
+		age=new double[]{0,0,1.59,1.59,1.57,1.47,1.47,1.16};
+		workers=new double[]{0,0.393,5.40,5.40,5.40,5.40,5.40};
+		cars=new double[]{0,0,6.96,6.96,6.96}; }
+		//index is APREW_norm field
 
-//newhouseholdchoice.mod
-//double[] asc={ 0.0, -9.47, -10.1, -10.2, -11.3, -12.4};
-//the index is the APERW_norm field (household size)
+	//typechoice.mod
+		if(UtilFunc==1){
+		asc=new double[]{1.33,0,-2.62};
+		educ=new double[]{0.142,0.590,0,-0.577};
+		age=new double[]{0,0,1.49,1.49,1.44,1.17,1.49,0.987};
+		workers=new double[]{0,0.473,5.49,5.49,5.49,5.49,5.49};
+		cars=new double[]{0,0,7.77,0,0}; }
+		//index is household type
+//</Utility function constants>
 
-//sizechoice.mod
-// double[] asc={1.91,0,-0.737,-0.993,-1.86,-2.63};
-// double[] educ={0,0.150,0.150,-0.657};
-// double[] age={0,0,1.59,1.59,1.57,1.47,1.47,1.16};
-// double[] workers={0,0.393,5.40,5.40,5.40,5.40,5.40};
-// double[] cars={0,0,6.96,6.96,6.96};
-//index is APREW_norm field
+//Initialization
+	int hhldfilelines, pfilelines, i, j;
+	String temp, currentline[];
+	Bipartite classcall = new Bipartite(); //run non-static methods
 
-//typechoice.mod
-double[] asc={1.33,0,-2.62};
-double[] educ={0.142,0.590,0,-0.577};
-double[] age={0,0,1.49,1.49,1.44,1.17,1.49,0.987};
-double[] workers={0,0.473,5.49,5.49,5.49,5.49,5.49};
-double[] cars={0,0,7.77,0,0};
-//index is household type
+	hhldfilelines=(classcall.LineNo(hhldfile)-1); //subtract 1 because we have headers
+	pfilelines=(classcall.LineNo(pfile)-1);
+	System.out.println("Number of households: "+hhldfilelines+"\nNumber of persons: "+pfilelines+"\n");
+	
+	double costmatrix[][] = new double[pfilelines][hhldfilelines];
+		//rows are people
+		//columns are households (head of household positions)
 
-//spousesizechoice.mod
-// double[] asc={0,1.12,0,0.0267,-0.821,-1.63};
-// double[] educ={0.502,0.380,0,-0.648};
-// double[] age={0,-1.35,0.681,0.681,0,-1.73,-2.57,-2.57};
-// double[] workers={0,0.731,0.563,0.563,0.563,0.563,0.563};
+	double households[][] = new double[hhldfilelines][hhldwidth];
+		//HHNR,ZGDE,GEBAEUDE_ID,APERW,APERW_norm,KAMS_bin,APKWL_bin,HMAT,HHTP,SHSD,SHFR,SHEN
 
-//spousechoice.mod
-// double[] asc={5.56};
-// double[] asc={12.8}; //just head-spouse relationship
 
-double costmatrix[][] = new double[pfilelines][hhldfilelines];
-//rows are people
-//columns are households (head of household positions)
 
-//import households
-double households[][] = new double[hhldfilelines][hhldwidth];
-//HHNR,ZGDE,GEBAEUDE_ID,APERW,APERW_norm,KAMS_bin,APKWL_bin,HMAT,HHTP,SHSD,SHFR,SHEN
 try{
 BufferedReader hhldreader = new BufferedReader(new FileReader(hhldfile));
 	temp=hhldreader.readLine(); //read and discard header
 	for(i=0;i<hhldfilelines;i++) { 
 		temp = hhldreader.readLine();
 		currentline = temp.split(",");
-		for(j=0;j<=(hhldwidth-1);j++) {
+		for(j=0;j<currentline.length;j++) {
 			households[i][j] = Double.parseDouble(currentline[j]);
 		} //inner for
 	} //outer for
@@ -102,7 +94,7 @@ BufferedReader preader = new BufferedReader(new FileReader(pfile));
 	for(i=0;i<pfilelines;i++) { 
 		temp = preader.readLine();
 		currentline = temp.split(",");
-		for(j=0;j<=5;j++) {
+		for(j=0;j<currentline.length;j++) {
 			persons[i][j] = Double.parseDouble(currentline[j]);
 		} //inner for
 	} //outer for
@@ -119,26 +111,29 @@ double rowsum[] = new double[pfilelines];
 for(i=0;i<pfilelines;i++){
 	for(j=0;j<hhldfilelines;j++){				
 		//sizechoice.mod
-			// if(households[j][7] == 1){
-				// Swiss=1;
-			// } else{
-				// Swiss=0;
-			// }
+		if(UtilFunc==0){
+			if(households[j][7] == 1){
+				Swiss=1;
+			} else{
+				Swiss=0;
+			}
 			
-			// if(households[j][10] == 1){
-				// French=1;
-			// } else{
-				// French=0;
-			// }
+			if(households[j][10] == 1){
+				French=1;
+			} else{
+				French=0;
+			}
 			
-			// if(households[j][4] == 0){
-				// costmatrix[i][j]=-asc[(int)households[j][4]] + 20;
-			// } else{
-				// costmatrix[i][j] = -(asc[(int)households[j][4]] + age[(int)persons[i][2]] - 0.328*persons[i][3] + educ[(int)persons[i][5]] - 0.419*Swiss - 0.393*French + workers[(int)households[j][5]] + cars[(int)households[j][6]]) + 20;
-			// }
+			if(households[j][4] == 0){
+				costmatrix[i][j]=-asc[(int)households[j][4]] + 20;
+			} else{
+				costmatrix[i][j] = -(asc[(int)households[j][4]] + age[(int)persons[i][2]] - 0.328*persons[i][3] + educ[(int)persons[i][5]] - 0.419*Swiss - 0.393*French + workers[(int)households[j][5]] + cars[(int)households[j][6]]) + 20;
+			}
+		}
 		//end sizechoice
 		
 		// typechoice.mod 
+		if(UtilFunc==1){
 			if(households[j][8] == 1000) {
 				AscTypeIndex=0;
 			}
@@ -166,45 +161,8 @@ for(i=0;i<pfilelines;i++){
 			} else{
 				costmatrix[i][j] = -(asc[AscTypeIndex] + age[(int)persons[i][2]] - 0.390*persons[i][3] + educ[(int)persons[i][5]] - 0.327*Swiss - 0.376*French + workers[(int)households[j][5]] + cars[(int)households[j][6]]) + 20;
 			}
+		}
 		// ****end typechoice
-		
-		//SpouseSizeChoice.mod
-			// if(households[j][6] == 1){
-				// Swiss=1;
-			// } else{
-				// Swiss=0;
-			// }
-			
-			// if(households[j][9] == 1){
-				// French=1;
-			// } else{
-				// French=0;
-			// }
-			
-			// if(households[j][3] == 1){
-				// costmatrix[i][j]=-asc[(int)households[j][3]] + 20;
-			// } else{
-				// costmatrix[i][j] = -(asc[(int)households[j][3]] + age[(int)persons[i][2]] + 0.534*persons[i][3] + educ[(int)persons[i][5]] - 0.410*Swiss - 0.146*French + workers[(int)households[j][4]]) + 20;
-				
-				// if(persons[i][5]==0){
-					// costmatrix[i][j]=1; //add bonus to prevent these from returning 0 results
-					// System.out.println("Person with education 0");
-					// System.out.println(educ[(int)persons[i][5]]);
-				// }
-			
-				// if(persons[i][2]==1 && (households[j][14]==1 || households[j][14]==2)){
-					// costmatrix[i][j]=costmatrix[i][j]-2; //add bonus to prevent these from returning 0 results
-				// }
-			// }
-			
-		//end spousesizechoice
-		
-		//SpouseChoice.mod
-		// costmatrix[i][j] = -(asc[0] + 0.00150*persons[i][2] + 0.589*persons[i][3] - 0.270*persons[i][5] - 1.97*Math.abs(households[j][14]-persons[i][2]) + 5.49*Math.abs(households[j][15]-persons[i][3]) - 2.84*Math.abs(households[j][17]-persons[i][5])) + 70;
-		// costmatrix[i][j] = -(asc[(int)households[j][3]] + 0.00953*persons[i][2] - 0.645*persons[i][3] - educ[(int)persons[i][5]]*1 + 0.000435*households[j][9] + 0.970*households[j][6] - 2);
-		
-		//just Head-Spouse relationship
-		// costmatrix[i][j] = -(asc[0] - 2.24*Math.abs(households[j][14]-persons[i][2]) + 9.95*Math.abs(households[j][15]-persons[i][3]) - 4.55*Math.abs(households[j][17]-persons[i][5])) + 50;
 		
 		//**all models
 		if(persons[i][2]==0){
